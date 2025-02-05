c691ac19374b0900bd6ca5ec9166e05d3c9dae4b
bacisormiruna <mirunabacisor06@gmail.com>
2025-02-06 00:23:11 +0200

    Added classes for the project that simulates a site for a store

---
+++ platform-project/src/test/java/com/project/platform_project/PlatformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.platform_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
